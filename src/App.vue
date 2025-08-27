517a575863ebce83e7e61c5fb179632a9281aae5
jamin <1743535684@qq.com>
2025-08-27 17:05:22 +0800

    设置antdv主题颜色、设置滑动条取值、添加注释。

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,52 @@
 <template>
-    <div class="mainView">
-        <div class="previewPart">
-            <div id="previewContainer">
-                <div id="backgroundContainer" ref="backgroundContainer">
-                    <div id="watermarkContainer" ref="watermarkContainer" @mousedown="RecordPos"
-                        @mouseup="{ dragging = false; }" @mousemove="SetTranslateSyle">
+    <a-config-provider :theme="{
+        token: {
+            colorPrimary: '#f1c58f',
+        },
+    }">
+        <div class="mainView">
+            <div class="previewPart">
+                <div id="previewContainer">
+                    <div id="backgroundContainer" ref="backgroundContainer">
+                        <div id="watermarkContainer" ref="watermarkContainer" @mousedown="RecordPos"
+                            @mouseup="{ dragging = false; }" @mousemove="SetDraggingTranslateSyle">
+                        </div>
                     </div>
                 </div>
             </div>
+            <div class="operatePart">
+                <a-row>
+                    <a-col :span="8" class="labelContainer">
+                        <span>水印横向偏移：</span>
+                    </a-col>
+                    <a-col :span="16">
+                        <a-slider v-model:value="rusultData.watermarkData.offsetX" :marks="xMarks" :max="xMax" @change="(value)=>SetSliderTranslateSyle(value,'x')"/>
+                    </a-col>
+                </a-row>
+                <a-row>
+                    <a-col :span="8" class="labelContainer">
+                        <span>水印纵向偏移：</span>
+                    </a-col>
+                    <a-col :span="16">
+                        <a-slider v-model:value="rusultData.watermarkData.offsetY" :marks="yMarks" :max="yMax"  @change="(value)=>SetSliderTranslateSyle(value, 'y')"/>
+                    </a-col>
+                </a-row>
+            </div>
+            <div class="paramPart">
+                {{ rusultData.backgroundData.scale }}
+                {{ rusultData.watermarkData.scale }}
+                {{ rusultData.watermarkData.offsetX }}
+                {{ rusultData.watermarkData.offsetY }}
+            </div>
         </div>
-        <div class="operatePart">
-            <a-slider :value="rusultData.watermarkData.offsetX"/>
-            <a-slider :value="rusultData.watermarkData.offsetY"/>
-        </div>
-        <div class="paramPart">
-            {{ rusultData.backgroundData.scale }}
-            {{ rusultData.watermarkData.scale }}
-            {{ rusultData.watermarkData.offsetX }}
-            {{ rusultData.watermarkData.offsetY }}
-        </div>
-    </div>
+    </a-config-provider>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { reactive, ref, onMounted, useTemplateRef } from 'vue'
 
-const rusultData = reactive({
+// 图片打水印的关键数据
+const rusultData = ref({
     backgroundData: {
         scale: 0,
     },
@@ -36,14 +57,33 @@ const rusultData = reactive({
     },
 });
 
+const backgroundContainerRef = useTemplateRef("backgroundContainer");
+const watermarkContainerRef = useTemplateRef("watermarkContainer");
+
+// 水印进入滑动过程的标志
 var dragging = false;
 
+// 水印开始滑动时的鼠标位置
 var lastCursorPosX = 0;
 var lastCursorPosY = 0;
 
+// 从css里读取的水印偏移量
 var watermarkTranslateX = 0;
 var watermarkTranslateY = 0;
 
+// 滑动条取值范围
+const xMarks = ref({
+    0: '0px',
+    100: '100px'
+});
+const yMarks = ref({
+    0: '0px',
+    100: '100px'
+});
+const xMax = ref(100)
+const yMax = ref(100)
+
+// 记录拖动开始
 function RecordPos(e) {
     dragging = true;
 
@@ -57,14 +97,48 @@ function RecordPos(e) {
     }
 };
 
-function SetTranslateSyle(e) {
+// 设置水印偏移
+function SetDraggingTranslateSyle(e) {
     if (dragging) {
-        rusultData.watermarkData.offsetX = watermarkTranslateX + (e.clientX - lastCursorPosX);
-        rusultData.watermarkData.offsetY = watermarkTranslateY + (e.clientY - lastCursorPosY);
+        rusultData.value.watermarkData.offsetX = watermarkTranslateX + (e.clientX - lastCursorPosX);
+        rusultData.value.watermarkData.offsetY = watermarkTranslateY + (e.clientY - lastCursorPosY);
 
-        e.target.style.transform = `translate(${rusultData.watermarkData.offsetX}px, ${rusultData.watermarkData.offsetY}px)`;
+        if (
+            rusultData.value.watermarkData.offsetX >= 0 &&
+            rusultData.value.watermarkData.offsetX <= xMax.value &&
+            rusultData.value.watermarkData.offsetY >= 0 &&
+            rusultData.value.watermarkData.offsetY <= yMax.value
+        ) {
+            e.target.style.transform = `translate(${rusultData.value.watermarkData.offsetX}px, ${rusultData.value.watermarkData.offsetY}px)`;
+
+        }
     }
 };
+
+function SetSliderTranslateSyle(value, flage) {
+    var lastWatermarkXY = watermarkContainerRef.value.style?.transform.match(/-?\d+/g) || [0,0];
+    var offsetX = parseInt(lastWatermarkXY[0]) || 0;
+    var offsetY = parseInt(lastWatermarkXY[1]) || 0;
+    if(flage === 'x'){
+        offsetX = value
+    }
+    if(flage === 'y'){
+        offsetY = value
+    }
+    watermarkContainerRef.value.style.transform = `translate(${offsetX}px, ${offsetY}px)`
+}
+
+onMounted(() => {
+    // 设置滑动条的水印偏移值范围
+    let height = backgroundContainerRef.value.clientHeight - watermarkContainerRef.value.clientHeight;
+    let width = backgroundContainerRef.value.clientWidth - watermarkContainerRef.value.clientWidth;
+    delete yMarks.value[100];
+    delete xMarks.value[100];
+    yMarks.value[height] = `${height}px`;
+    xMarks.value[width] = `${width}px`;
+    yMax.value = height;
+    xMax.value = width;
+});
 
 </script>
 
@@ -103,6 +177,7 @@ function SetTranslateSyle(e) {
     background-size: contain;
     background-position: center;
     background-repeat: no-repeat;
+    transform: translate(0px,0px);
 }
 
 #watermarkContainer:hover {
@@ -115,6 +190,14 @@ function SetTranslateSyle(e) {
     filter: none;
 }
 
-.paramPart{
+.operatePart {
+    padding: 20px;
 }
+
+.labelContainer {
+    align-content: center;
+    text-align: center;
+}
+
+.paramPart {}
 </style>
